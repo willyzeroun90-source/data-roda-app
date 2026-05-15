1f1c1c8485f637cdfc45432acab499bb67ca2198
willyzeroun90-source <willy.zeroun90@gmail.com>
2026-05-15 21:23:18 +0700

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/com/dataroda/app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dataroda.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://dataroda.my.id");
+    }
+}
