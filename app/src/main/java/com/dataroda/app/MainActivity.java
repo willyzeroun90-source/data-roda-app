85d1c13c72d60194edd1a4adeaac0ef7765bb771
willyzeroun90-source <willy.zeroun90@gmail.com>
2026-05-16 15:53:09 +0700

    Update MainActivity.java

--- app/src/main/java/com/dataroda/app/MainActivity.java
+++ app/src/main/java/com/dataroda/app/MainActivity.java
@@ -1,13 +1,16 @@
 package com.dataroda.app;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    WebView webView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +19,58 @@ public class MainActivity extends Activity {
         webView = new WebView(this);
         setContentView(webView);
 
+        // Setting WebView
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(true);
 
-        webView.setWebViewClient(new WebViewClient());
+        // Tangani link WhatsApp dan link external
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                String url = request.getUrl().toString();
+
+                // Jika link WhatsApp
+                if (url.startsWith("whatsapp://")) {
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(url));
+                        intent.setPackage("com.whatsapp");
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+
+                // Jika link telpon
+                if (url.startsWith("tel:")) {
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+
+                // Jika link biasa tetap buka di WebView
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        // Website utama
         webView.loadUrl("https://dataroda.my.id");
+    }
+
+    // Tombol back
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
